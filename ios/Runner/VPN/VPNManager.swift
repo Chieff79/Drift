78b226bfc58eb2caacddd7110a927a548fe70bfd
Chieff79 <chieff1.rk@gmail.com>
2026-03-17 15:53:13 +0000

    fix: correct Network Extension bundle identifier (.tunnel instead of .HiddifyPacketTunnel)
    - VPNManager was referencing com.drift.vpn.HiddifyPacketTunnel but the actual tunnel extension is com.drift.vpn.tunnel
    - This caused startService failure when trying to connect to VPN
    - Also updated VPN description to Drift VPN
    - Bumped to build 40105

--- ios/Runner/VPN/VPNManager.swift
+++ ios/Runner/VPN/VPNManager.swift
@@ -99,10 +99,10 @@ class VPNManager: ObservableObject {
             }
             let newManager = NETunnelProviderManager()
             let `protocol` = NETunnelProviderProtocol()
-            `protocol`.providerBundleIdentifier = Bundle.main.baseBundleIdentifier + ".HiddifyPacketTunnel"
+            `protocol`.providerBundleIdentifier = Bundle.main.baseBundleIdentifier + ".tunnel"
             `protocol`.serverAddress = "localhost"
             newManager.protocolConfiguration = `protocol`
-            newManager.localizedDescription = "Hiddify"
+            newManager.localizedDescription = "Drift VPN"
             try await newManager.saveToPreferences()
             try await newManager.loadFromPreferences()
             self.manager = newManager
